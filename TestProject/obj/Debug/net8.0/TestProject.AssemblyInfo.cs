6f246a62ea170d0a7fa517c85866ccd8aac7182c
lucienlmp <lucienlmp@hotmail.com>
2024-05-23 14:55:03 +0100

    compete subscription if statement excercise

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f95835313a929ce20f2ce2386df129d9bf0cc2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736f97b9ffef33daca5df9fd10e81694295df7c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
